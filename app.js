1218e4dc1f13bc00dcc55734ac0b16b44a7c83f6
he <939158007@qq.com>
2017-10-25 17:47:15 +0800

    express socket.io

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path')
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+
+});
+io.on('connection', (socket) => {
+  console.log('socket connected')
+  socket.on('disconnect', () => {
+    console.log('disconnect')
+  })
+  socket.on('chat message',(msg)=>{
+    console.log('msg:'+msg)
+    io.emit('chat message',msg)
+  });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = http.listen(app.get('port'), function() {
+  console.log('start at port:' + server.address().port);
+});
